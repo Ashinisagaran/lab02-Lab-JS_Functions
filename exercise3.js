152a4b8f27a211dadeb5fbe37432d9f0b77b846d
Ashini Sagaran <20087371@mail.wit.ie>
2022-04-13 12:32:49 +0100

    Exercise 3.

---
+++ exercise3.js
@@ -0,0 +1,10 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const uncompleted = json.filter((todo) => ! todo.completed
+     ).map(todo => ({userID: todo.userID, title: todo.title }))
+      console.log(uncompleted)
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
